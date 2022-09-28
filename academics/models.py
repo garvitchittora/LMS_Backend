0256790fa4eb78d958f5048da92504d6a0898065
pankaj1707k <pankaj.1707k@gmail.com>
2022-09-28 23:20:02 +0530

    feat(academics): Add model Subject
    Register model on admin site.
    Create migration file.

--- academics/models.py
+++ academics/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from students.models import Student
+from users.models import User
 
-# Create your models here.
+
+class Subject(models.Model):
+    """
+    Contains information of a subject such as title, description, etc.
+    """
+    title = models.CharField(_("title"), max_length=100, unique=True)
+    description = models.TextField(_("description"), blank=True)
+    teachers = models.ManyToManyField(User, related_name="subjects")
+    students = models.ManyToManyField(Student, related_name="subjects")
+
+    def __str__(self) -> str:
+        return self.title
