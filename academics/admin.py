23993a1ad09fad0eaf5af83d40d2662c6c099d2f
pankaj1707k <pankaj.1707k@gmail.com>
2022-09-29 16:28:47 +0530

    feat(academics): Add model Class
    Register it on admin site and create corresponding migration file

--- academics/admin.py
+++ academics/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from academics.models import Subject
+from academics.models import Class, Subject
 
 admin.site.register(Subject)
+admin.site.register(Class)
--- academics/models.py
+++ academics/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import UniqueConstraint
 from django.utils.translation import gettext_lazy as _
 from students.models import Student
 
@@ -17,3 +18,22 @@ class Subject(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+
+class Class(models.Model):
+    """
+    Contains information of a class. A unique class is identified by a 
+    number from 1-12 (or LKG, UKG, Nursery, etc.) and a section (A, B, C, etc.)
+    """
+    classname = models.CharField(_("class name"), max_length=10)
+    section = models.CharField(_("section"), max_length=10)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=["classname", "section"], name="class_unique_constraint"
+            )
+        ]
+    
+    def __str__(self) -> str:
+        return f"{self.classname} {self.section}"
