6ef0dbdc06d7f8be57015193c8e40199ea787e5c
pankaj1707k <pankaj.1707k@gmail.com>
2022-09-29 23:16:34 +0530

    feat(academics): Add models AcademicSession, Examination, Score
    Implicit: Register on admin site

--- academics/admin.py
+++ academics/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 
-from academics.models import Class, Subject
+from academics.models import (AcademicSession, Class, Examination, Score,
+                              Subject)
 
 admin.site.register(Subject)
 admin.site.register(Class)
+
+
+@admin.register(AcademicSession)
+class AcademicSessionAdmin(admin.ModelAdmin):
+    """
+    Encapsulate all admin options and functionality for AcademicSession model.
+    """
+    model = AcademicSession
+    list_display = ("start", "end")
+
+
+@admin.register(Examination)
+class ExaminationAdmin(admin.ModelAdmin):
+    """
+    Encapsulate all admin options and functionality for Examination model.
+    """
+    model = Examination
+    list_display = ("title", "term", "session", "max_score")
+
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    """
+    Encapsulate all admin options and functionality for Score model.
+    """
+    model = Score
+    list_display = ("student", "subject", "examination", "score")
--- academics/models.py
+++ academics/models.py
@@ -38,3 +38,55 @@ class Class(models.Model):
     
     def __str__(self) -> str:
         return f"{self.classname} {self.section}"
+
+
+class AcademicSession(models.Model):
+    """
+    Contains start and end date of an academic session.
+    """
+    start = models.DateField(_("start date"))
+    end = models.DateField(_("end date"))
+    
+    def __str__(self) -> str:
+        return f"{self.start.year}-{self.end.year}"
+
+
+class Examination(models.Model):
+    """
+    Contains session (eg: 2021-22), term (eg: 1, 2) and exam name (eg: PT-1, HY).
+    """
+    term = models.CharField(_("term"), max_length=1)
+    title = models.CharField(_("title"), max_length=10)
+    max_score = models.PositiveSmallIntegerField(_("maximum score"))
+    session = models.ForeignKey(AcademicSession, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=["term", "title", "session"], name="exam_unique"
+            )
+        ]
+    
+    def __str__(self) -> str:
+        return f"{self.term} {self.title} {self.session}"
+
+
+class Score(models.Model):
+    """
+    Contains the score of a student in an examination.
+    """
+    score = models.PositiveSmallIntegerField(_("score"), default=0)
+    examination = models.ForeignKey(Examination, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=["examination", "student", "subject"], name="student_score"
+            )
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.student} {self.subject} {self.examination}"
